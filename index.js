a84ba42cec5edba57a5d2cffd4b55ffdf73ccb8f
d3tu <69614422+d3tu@users.noreply.github.com>
2022-01-22 22:22:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
-const nodeFetch = require('node-fetch'),
-	cheerio = require('cheerio'),
-	stringSimilarity = require('string-similarity');
-
-function fetch(url) {
-	return nodeFetch('https://www.dicio.com.br' + url).then(res => res.text());
-}
-
-module.exports = q =>
-	fetch(
-		'/pesquisa.php?' +
-			new URLSearchParams({
-				q
-			})
-	).then(response => {
-		const $ = cheerio.load(response),
-			urls = $('#wrapper > .container a')
-				.map((i, e) => e.attribs.href)
-				.get(),
-			url = stringSimilarity.findBestMatch(q, urls).bestMatch.target;
-		return fetch(url).then(res => {
-			const $$ = cheerio.load(res);
-			return {
-				word: $$('h1[itemprop="name"]').text(),
-				definition: $$(`.sg-social`).attr('data-text'),
-				url,
-				urls
-			};
-		});
-	});
+const fetch = require('node-fetch').default,
+  cheerio = require('cheerio'),
+  equal = require('string-similarity'),
+  URL = 'https://www.dicio.com.br';
+module.exports = (q = '') => {
+  if (!q || typeof q != 'string') throw new Error('the first argument is not a string');
+  return fetch(`${URL}/pesquisa.php?${new URLSearchParams({ q })}`)
+    .then(res => res.text()).then(res => {
+      let $ = cheerio.load(res),
+        urls = $('#wrapper > .container a').map((_, e) => e.attribs.href).get(),
+        url = equal.findBestMatch(q, urls);
+      return fetch(URL + url).then(res => res.json()).then(res => {
+        $ = cheerio.load(res);
+        return {
+          word: $$('h1[itemprop="name"]').text(),
+          definition: $$(`.sg-social`).attr('data-text'),
+	  url, urls
+        };
+      });
+    });
+};
